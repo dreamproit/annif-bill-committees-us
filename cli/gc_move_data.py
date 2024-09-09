334f2e186bf4a1711c07c96e83d6e470738c15d2
Andrii Kovalov <45897775+BorodaUA@users.noreply.github.com>
2024-09-09 13:33:21 +0300

    [BML-31]Investigate hf or annif models deployment with streamlit (#3)
    * [BML-26]investigate annif tool for classification bill committees us dataset (#1)
    
    * annif project training initial setup.
    
    * nn-ansemble project added.
    
    * Created using Colab
    
    * extra notebook removed
    
    ---------
    
    Co-authored-by: Oleksandr Boiko <django.develop@gmail.com>
    
    * streamlit app inital setup for annif models.
    
    * data folder removed.
    
    * data folder download from gcs via streamlit initial setup.
    
    * annif data download inside st app run.
    
    * punkt download added in st app.
    
    * static annif models selection.
    
    * duplicate committees updated in csv.
    
    * docs updated with streamlit app info.
    
    ---------
    
    Co-authored-by: Oleksandr Boiko <django.develop@gmail.com>

---
+++ cli/gc_move_data.py
@@ -0,0 +1,115 @@
+import click
+import gcsfs
+from datetime import datetime
+import pathlib
+import shutil
+
+import os
+import zipfile
+from google.cloud import storage
+import streamlit as st    
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file), 
+                       os.path.relpath(os.path.join(root, file), 
+                                       os.path.join(path, '..')))
+
+
+def get_st_gcs_secrets():
+    gcs_secrets_dict = st.secrets['connections']['gcs']
+    return gcs_secrets_dict
+
+
+def create_cloud_client_with_streamlit():
+    gcs_secrets_dict = get_st_gcs_secrets()
+    client = storage.Client.from_service_account_info(gcs_secrets_dict)
+    return client
+
+
+def archive_data(source, archive_folder: str = './archives'):
+    now_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    archives_folder_path = pathlib.Path(archive_folder)
+    source_path = pathlib.Path(source)
+    archives_folder_path.mkdir(parents=True, exist_ok=True)
+    archive_file_name = f"{now_time}_{source_path.stem}.zip"
+    archive_dest_path = archives_folder_path / archive_file_name
+    with zipfile.ZipFile(archive_dest_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipdir(source, zipf)
+    print(f"Succesfully archived {source} to {archive_dest_path}.")
+    return archive_dest_path
+
+
+def upload_to_gc(source, project_id: str, bucket_name: str, creds_file_path: str):
+    source_file_path = pathlib.Path(source)
+    cloud_storage_client = create_cloud_client_with_streamlit()
+    bucket = cloud_storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_file_path.name)
+    blob.upload_from_filename(source_file_path)
+    print(f'Uploaded {source_file_path} to {bucket_name}.')
+    return source_file_path
+
+
+def upload_data(source, project_id: str, bucket_name: str, creds_file_path: str, archive: bool):
+    if archive:
+        to_upload_filepath = archive_data(source)
+    else:
+        to_upload_filepath = source
+    upload_to_gc(to_upload_filepath, project_id, bucket_name, creds_file_path)
+
+
+def download_from_gc(project_id: str, bucket_name: str, creds_file_path: str, destination:str = './'):
+    cloud_storage_client = create_cloud_client_with_streamlit()
+    bucket = cloud_storage_client.get_bucket(bucket_name)
+    latest_blob = None
+    sorted_blobs = sorted(bucket.list_blobs(), key=lambda blob: blob.time_created)
+    for blob in sorted_blobs:
+        latest_blob = blob
+    if not latest_blob:
+        print(f"No files in bucket {bucket_name}.")
+        return
+    destination_file_path = pathlib.Path(destination) / latest_blob.name
+    latest_blob.download_to_filename(destination_file_path)
+    print(f'Downloaded {latest_blob.name} to {destination_file_path}.')
+    return destination_file_path
+
+
+def download_data(project_id: str, bucket_name: str, creds_file_path: str, archive: bool, destination):
+    dowloaded_filename = download_from_gc(project_id, bucket_name, creds_file_path, destination)
+    if archive:
+        with zipfile.ZipFile(dowloaded_filename, 'r') as zip_ref:
+            zip_ref.extractall(destination)
+        print(f"Extracted {dowloaded_filename} to {destination}.")
+
+
+def gc_move_data(mode: str, source: str = './data', destination: str = './', project_id: str = None, bucket_name: str = None, creds_file_path: str = None, archive: bool = True):
+    if mode == 'upload':
+        upload_data(source, project_id, bucket_name, creds_file_path, archive)
+    elif mode == 'download':
+        download_data(project_id, bucket_name, creds_file_path, archive, destination)
+    else:
+        print(f"Invalid mode: {mode}")
+
+
+@click.command()
+@click.option('--mode', 'mode', help='Mode of the move data command.', default='upload')
+@click.option('--source', 'source', help='Source folder or file to upload.', default='./data')
+@click.option('--destination', 'destination', help='Destination folder or file to download.', default='./')
+@click.option('--project-id', 'project_id', help='The project id of the google cloud project.', default='models')
+@click.option('--bucket-name', 'bucket_name', help='The name of the google cloud storage bucket.', default='annif-models')
+@click.option('--creds-file-path', 'creds_file_path', help='The filepath to google creds .json file', default='./creds.json')
+@click.option('--archive', 'archive', help='Whether to archive the data.', default=False, is_flag=True)
+def main(mode: str, source: str, destination: str, project_id: str, bucket_name: str, creds_file_path: str, archive: bool):
+    """Move data from local to google cloud storage or vice versa."""
+    try:
+        gc_move_data(mode, source, destination, project_id, bucket_name, creds_file_path, archive)
+    except Exception as e:
+        print(f"Error: {e}")
+        raise e
+
+
+if __name__ == '__main__':
+    main()
---
+++ st_annif.py
@@ -0,0 +1,130 @@
+import os
+import sys
+import streamlit as st
+import pandas as pd
+import numpy as np
+from annif import rest, create_flask_app
+import pathlib
+import nltk
+sys.path.append(os.getcwd())  # noqa: E402
+from cli.gc_move_data import gc_move_data, get_st_gcs_secrets
+
+
+def list_annif_projects():
+    app = create_flask_app
+    with app().app_context():
+        projects = rest.list_projects()
+    return projects
+
+
+def get_annif_projects():
+    projects = list_annif_projects()
+    models = [i['project_id'] for i in projects[0]['projects']]
+    return models
+
+
+def run_annif_suggest(model_name, input_text):
+    app = create_flask_app
+    with app().app_context():
+        results = rest._suggest(project_id=model_name, documents=[{'text': input_text}], parameters={})
+    return results
+
+
+def main():
+    ANNIF_DATA_FOLDER_PATH = './data'
+    annif_data_filepath = pathlib.Path(ANNIF_DATA_FOLDER_PATH)
+    if not annif_data_filepath.exists():
+        gcs_secrets = get_st_gcs_secrets()
+        gc_move_data(
+            mode='download',
+            destination='./',
+            bucket_name=gcs_secrets['project_id'],
+            archive=True
+        )
+    nltk.download('punkt')
+    if not "valid_inputs_received" in st.session_state:
+        st.session_state["valid_inputs_received"] = False
+
+    st.set_page_config(
+        layout="centered", page_title="US Congress Bill's committees suggester.", page_icon="📚"
+    )
+    original_title = "<p style='font-family:Courier; color:Black; font-size: 20px;'>US Congress bill's text committees suggester</p>"
+    st.markdown(original_title, unsafe_allow_html=True)
+    # models = get_annif_projects()
+    models = ['cm-mllm-en', 'cm-parabel-en', 'cm-bonsai-en', 'cm-nn-ensemble-en']
+    selected_model = st.selectbox(
+        "Please select model:",
+        models,
+        index=2,
+        disabled=True,
+    )
+
+    st.write("")
+    st.markdown(
+        f"""
+
+    Classify Bill's committees using the model: "{selected_model}" trained on the US Congress dataset.
+
+    ⚠️ Please note that long bill's text(more than ~40 pdf pages) may take a lot of resources and cause the app to restart.
+
+    """
+    )
+    st.write("")
+    with st.form(key="my_form"):
+
+        text = st.text_area(
+            "Enter Bill's text to classify",
+            height=200,
+            key="1",
+        )
+
+        text = text.strip()
+
+
+        submit_button = st.form_submit_button(label="Submit")
+    
+    if not submit_button and not st.session_state.valid_inputs_received:
+        st.stop()
+
+    elif submit_button and not text:
+        st.warning("Bill's text cannot be empty.")
+        st.session_state.valid_inputs_received = False
+        st.stop()
+    
+    elif submit_button or st.session_state.valid_inputs_received:
+
+        if submit_button:
+
+            st.session_state.valid_inputs_received = True
+
+            results = run_annif_suggest(selected_model, text)
+
+            df = pd.DataFrame(results[0]['results'])
+            f = [f"{row:.2%}" for row in df["score"]]
+            df["Classification scores"] = f
+
+            df.rename(columns={"label": "Committee", "uri": "URL"}, inplace=True)
+
+            df.drop(["notation", "score"], inplace=True, axis=1)
+
+            df.index = np.arange(1, len(df) + 1)
+
+            df = df[['Committee', 'Classification scores', 'URL']]
+
+            st.success("✅ Done!")
+
+            st.caption("")
+            st.markdown("### Check the results!")
+            st.caption("")
+
+            st.data_editor(
+                df,
+                column_config={
+                    "URL": st.column_config.LinkColumn("URL")
+                },
+                hide_index=True,
+            )
+
+
+if __name__ == "__main__":
+    main()
